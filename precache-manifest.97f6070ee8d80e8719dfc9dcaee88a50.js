06c298790b5ef14a172174d906557d77c42f6c22
Logan Moss <lrdjmoss@gmail.com>
2020-04-30 18:42:28 -0700

    Updates

---
+++ precache-manifest.97f6070ee8d80e8719dfc9dcaee88a50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de8528b0777a24c906c859d791e2e7ab",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "87c21babc5b1545073b6",
+    "url": "/ClickyGame/static/css/main.2900e686.chunk.css"
+  },
+  {
+    "revision": "673954c700adb3913765",
+    "url": "/ClickyGame/static/js/2.21e3f792.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClickyGame/static/js/2.21e3f792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87c21babc5b1545073b6",
+    "url": "/ClickyGame/static/js/main.866d78fb.chunk.js"
+  },
+  {
+    "revision": "883b763d82ab0e0c736f",
+    "url": "/ClickyGame/static/js/runtime-main.6e0552f1.js"
+  }
+]);

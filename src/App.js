031e60b9d03444aec2cfdb8843b3ee7055942aa1
Logan M <56891756+Loganrdj@users.noreply.github.com>
2020-07-28 17:15:51 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
     if (this.state.score >= this.state.highScore){
       this.setState({highScore: this.state.score + 1});
     }
-    if (this.state.score === 12){
+    if (this.state.score === 11){
       alert("You've won! Congratulations! I don't have anything to offer you, unfortunately.. Have a virtual high-five! *Smack*")
       this.resetScore();
     }
